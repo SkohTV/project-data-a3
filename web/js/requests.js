4fbeb6ea3c2c0bd3fafe84a3b514800a4266315a
CorentinMre <72888555+CorentinMre@users.noreply.github.com>
2025-06-24 15:12:18 +0200

    request filter params

--- web/js/requests.js
+++ web/js/requests.js
@@ -6,35 +6,39 @@ function applyFilters() {
 
   params.append(
     "longueur_min",
-    filters.longueur?.min ?? filterData.longueur[1]
+    filters.longueur?.min ?? (typeof filterData !== "undefined" ? filterData.longueur[1] : "")
   );
   params.append(
     "longueur_max",
-    filters.longueur?.max ?? filterData.longueur[0]
+    filters.longueur?.max ?? (typeof filterData !== "undefined" ? filterData.longueur[0] : "")
   );
 
-  params.append("largeur_min", filters.largeur?.min ?? filterData.largeur[1]);
-  params.append("largeur_max", filters.largeur?.max ?? filterData.largeur[0]);
+  params.append("largeur_min", filters.largeur?.min ?? (typeof filterData !== "undefined" ? filterData.largeur[1] : ""));
+  params.append("largeur_max", filters.largeur?.max ?? (typeof filterData !== "undefined" ? filterData.largeur[0] : ""));
 
   if (filters.temps) {
     params.append("temps_min", Date.parse(filters.temps.min));
     params.append("temps_max", Date.parse(filters.temps.max));
-  } else {
+  } else if (typeof dateRange !== "undefined") {
     params.append("temps_min", dateRange[0]);
     params.append("temps_max", dateRange[1]);
+  } else {
+    params.append("temps_min", "");
+    params.append("temps_max", "");
   }
 
   params.append("transceiver_class", filters.transceiver ?? "");
 
   params.append("status_code", filters.status_code ?? "");
-
-  params.append("mmsi", filters.mmsi ?? "");
-
-  ajaxRequest(
-    "GET",
-    "php/all_points_donnee?" + params.toString(),
-    function (response) {
-      console.log(response);
-    }
-  );
+  if (typeof ajaxRequest === "function") {
+    ajaxRequest(
+      "GET",
+      "php/all_points_donnee?" + params.toString(),
+      function (response) {
+        console.log(response);
+      }
+    );
+  } else {
+    console.error("ajaxRequest function is not defined.");
+  }
 }
