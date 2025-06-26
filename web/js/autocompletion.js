170d5a4817f3df756e9a99d740282fcb41ee1de9
mkkuu <129859952+mkkuu@users.noreply.github.com>
2025-06-26 11:36:29 +0200

    autocompletion

--- web/js/autocompletion.js
+++ web/js/autocompletion.js
@@ -19,3 +19,25 @@ document.getElementById("filter-mmsi").oninput = () => {
 
   }
 }
+
+document.getElementById("point-mmsi").oninput = () => {
+  const mmsi = document.getElementById("filter-mmsi").value.trim();
+  if (mmsi.length >= 3) {
+    
+    
+    ajaxRequest('GET', `php/requests.php/all_mmsi?mmsi=${mmsi}`, (response) => {
+        const dataList = document.getElementById("point-mmsi-list");
+        dataList.innerHTML = "";
+        response.forEach((item) => {
+            const option = document.createElement("option");
+            option.value = item;
+            dataList.appendChild(option);
+        });
+        }
+    );
+
+
+
+  }
+}
+
