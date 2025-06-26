456d55fc65d1d6d435d01b09b98edc5c78d24bfc
mkkuu <129859952+mkkuu@users.noreply.github.com>
2025-06-26 11:38:46 +0200

    fix

--- web/js/autocompletion.js
+++ web/js/autocompletion.js
@@ -14,17 +14,12 @@ document.getElementById("filter-mmsi").oninput = () => {
         });
         }
     );
-
-
-
   }
 }
 
 document.getElementById("point-mmsi").oninput = () => {
-  const mmsi = document.getElementById("filter-mmsi").value.trim();
+  const mmsi = document.getElementById("point-mmsi").value.trim();
   if (mmsi.length >= 3) {
-    
-    
     ajaxRequest('GET', `php/requests.php/all_mmsi?mmsi=${mmsi}`, (response) => {
         const dataList = document.getElementById("point-mmsi-list");
         dataList.innerHTML = "";
@@ -35,9 +30,6 @@ document.getElementById("point-mmsi").oninput = () => {
         });
         }
     );
-
-
-
   }
 }
 
