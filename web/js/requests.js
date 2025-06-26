42609a833e2ff90642db6f385839974b49e493ac
CorentinMre <72888555+CorentinMre@users.noreply.github.com>
2025-06-26 11:01:47 +0200

    up

--- web/js/requests.js
+++ web/js/requests.js
@@ -517,25 +517,5 @@ function exportData() {
 
 
 
-document.getElementById("filter-mmsi").oninput = () => {
-  const mmsi = document.getElementById("filter-mmsi").value.trim();
-  if (mmsi.length >= 3) {
-    
-    
-    ajaxRequest('GET', `php/requests.php/all_mmsi?mmsi=${mmsi}`, (response) => {
-        const dataList = document.getElementById("filter-mmsi-list");
-        dataList.innerHTML = "";
-        response.forEach((item) => {
-            const option = document.createElement("option");
-            option.value = item;
-            dataList.appendChild(option);
-        });
-        }
-    );
-
-
-
-  }
-}
 
 
